011ea34cdd4b91b2ae17be5fbe4115161a42476b
zfirelight <90018684+zfirelight@users.noreply.github.com>
2021-09-11 01:45:15 -0400

    Add files via upload

---
+++ B7B8303E.js
@@ -0,0 +1,33 @@
+function myFunction() { 
+    document.getElementById("mp4").src = "https://app.koofr.net/content/links/71d8e371-151c-4fe0-908a-31eb4345d75d/files/get/09092021%20(1).m4a?path=%2F";
+    document.getElementById("video").load();
+    document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/1.png";
+    document.getElementById("video").addEventListener('ended',myFunction2,false);
+}
+
+function myFunction2() { 
+    document.getElementById("mp4").src = "https://app.koofr.net/content/links/67ab54a8-6bad-4948-81f4-44d0d84024ff/files/get/09092021%20(2).m4a?path=%2F";
+    document.getElementById("video").load();
+    document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/2.png";
+    document.getElementById("video").addEventListener('ended',myFunction3,false);
+}
+
+function myFunction3() { 
+    document.getElementById("mp4").src = "https://app.koofr.net/content/links/de3e6651-81fd-41c0-b134-db261e2b960c/files/get/09092021%20(3).m4a?path=%2F";
+    document.getElementById("video").load();
+    document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/3.png";
+    document.getElementById("video").addEventListener('ended',myFunction4,false);
+}
+
+function myFunction4() { 
+    document.getElementById("mp4").src = "https://app.koofr.net/content/links/ffdfd92b-b45d-4152-9597-375ba05f2a9a/files/get/09092021%20(4).m4a?path=%2F";
+    document.getElementById("video").load();
+    document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/4.png";
+    document.getElementById("video").addEventListener('ended',myFunction5,false);
+}
+
+function myFunction5() { 
+    document.getElementById("mp4").src = "";
+    document.getElementById("video").load();
+    document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/refresh.png";
+}
