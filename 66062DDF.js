a89ea4bceb4ad3547075c7a9032cdb1faa5d98b2
zfirelight <90018684+zfirelight@users.noreply.github.com>
2022-02-05 15:58:49 -0500

    Add files via upload

--- 66062DDF.js
+++ 66062DDF.js
@@ -13,14 +13,14 @@ function myFunction2() {
 }
 
 function myFunction3() { 
-    document.getElementById("mp4").src = "https://app.koofr.net/content/links/81349ea0-0394-4227-654a-4cfbe58b751f/files/get/02032022%20(3).m4a?path=%2F";
+    document.getElementById("mp4").src = "https://app.koofr.net/content/links/0b8badc8-01e7-42e4-438f-c21815741146/files/get/02032022%20(3).m4a?path=%2F";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/3.png";
     document.getElementById("video").addEventListener('ended',myFunction4,false);
 }
 
 function myFunction4() { 
-    document.getElementById("mp4").src = "https://app.koofr.net/content/links/3bf187c9-f62b-4b76-413f-3db892542bd3/files/get/02032022%20(4).m4a?path=%2F";
+    document.getElementById("mp4").src = "https://app.koofr.net/content/links/628dfe6e-ba06-46b2-7200-c38a5eb71c33/files/get/02032022%20(4).m4a?path=%2F";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/4.png";
     document.getElementById("video").addEventListener('ended',myFunction5,false);
