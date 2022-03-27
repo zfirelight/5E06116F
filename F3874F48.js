e3d2e3d3bbd91600c896cddb7a619cf7837b7253
zfirelight <90018684+zfirelight@users.noreply.github.com>
2022-03-27 11:18:55 -0400

    Add files via upload

--- F3874F48.js
+++ F3874F48.js
@@ -1,26 +1,26 @@
 function myFunction() { 
-    document.getElementById("mp4").src = "https://app.koofr.net/content/links/913eb25a-0800-4627-90a0-5dc48da20fbc/files/get/01022021%20(1).m4a?path=%2F";
+    document.getElementById("mp4").src = "https://app.koofr.net/content/links/8a17409d-074d-4f79-736d-5c5911c61727/files/get/01022022%20(1).m4a?path=%2F";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/1.png";
     document.getElementById("video").addEventListener('ended',myFunction2,false);
 }
 
 function myFunction2() { 
-    document.getElementById("mp4").src = "https://app.koofr.net/content/links/dd554820-be6d-4c2e-b68a-68216ce30948/files/get/01022021%20(2).m4a?path=%2F";
+    document.getElementById("mp4").src = "https://app.koofr.net/content/links/c5fef6b4-9ec6-44e5-63e4-ce6ae569522a/files/get/01022022%20(2).m4a?path=%2F";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/2.png";
     document.getElementById("video").addEventListener('ended',myFunction3,false);
 }
 
 function myFunction3() { 
-    document.getElementById("mp4").src = "https://app.koofr.net/content/links/697eeca9-acae-424f-a1ad-6a7af3e9b520/files/get/01022021%20(3).m4a?path=%2F";
+    document.getElementById("mp4").src = "https://app.koofr.net/content/links/02add702-987a-401a-77d4-14ceded85522/files/get/01022022%20(3).m4a?path=%2F";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/3.png";
     document.getElementById("video").addEventListener('ended',myFunction4,false);
 }
 
 function myFunction4() { 
-    document.getElementById("mp4").src = "https://app.koofr.net/content/links/de769ea8-b563-429f-98a5-0ef8062be1e3/files/get/01022021%20(4).m4a?path=%2F";
+    document.getElementById("mp4").src = "https://app.koofr.net/content/links/38cf4cff-e3b9-4c72-6e13-b105ceecc8e7/files/get/01022022%20(4).m4a?path=%2F";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/4.png";
     document.getElementById("video").addEventListener('ended',myFunction5,false);
